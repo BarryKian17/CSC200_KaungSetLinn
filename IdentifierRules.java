b9761c086c67337b29f4fa59ca8b9080cd1e017a
Kaung Set Linn <138768657+BarryKian17@users.noreply.github.com>
2025-09-04 13:20:43 +0800

    IdentifierRules.java

--- IdentifierRules.java
+++ IdentifierRules.java
@@ -1,63 +1,4 @@
-// /**********************************************************************
-  
-//   Program Title : Demonstrating Variable Data Types in Java with Sizes
-//   File Name: "DataTypeSize.java"
-  
-//   Programmer : Sathish Sarma
-//   Contact Details : sathish.sarma@newinti.edu.my
-//   Date : 28 August 2025
-  
-//  **********************************************************************/
 
-// public class DataTypeSize {
-//     public static void main(String[] args) {
-//         System.out.println("Demonstrating Data Type Size in Java");
-//         System.out.println("-----------------------------------------");
-        
-//         byte b = 120;
-//         short s = 32000;
-//         int i = 123456789;
-//         double d = 19.99;
-//         char c = 'A';
-//         long l = 9876543210L;
-//         float f = 5.75f;
-		
-// 		//Instruction
-// 		System.out.println("This application shows the basic code that is needed to calculate data type size, the range (min & max value) for each data type.\n");
-		
-// 		//Finding data type size using BYTES
-// 		System.out.println("The Variable b is bytes data type has the value of " + b + "\nSize of bytes: " + Byte.BYTES + " bytes.");
-// 		System.out.println("Size of bytes: " + Byte.SIZE + " bytes.\n");
-		
-// 		System.out.println("The Variable s is short data type has the value of " + s + "\nSize of short: " + Short.BYTES + " bytes.");
-// 		System.out.println("Size of short: " + Short.SIZE + " bytes.\n");
-		
-// 		System.out.println("The Variable i is int data type has the value of " + i + "\nSize of int: " + Integer.BYTES + " bytes.");
-// 		System.out.println("Size of int: " + Integer.SIZE + " bytes.\n");
-		
-// 		System.out.println("The Variable c is Character data type has the value of " + c + "\nSize of char: " + Character.BYTES + " bytes.");
-// 		System.out.println("Size of char: " + Character.SIZE + " bytes.\n");
-		
-// 		System.out.println("The Variable d is Double data type has the value of " + d + "\nSize of double: " + Double.BYTES + " bytes.");
-// 		System.out.println("Size of double: " + Double.SIZE + " bytes.\n");
-				
-// 		System.out.println("The Variable l is Long data type has the value of " + l + "\nSize of long: " + Long.BYTES + " bytes.");
-// 		System.out.println("Size of long: " + Long.SIZE + " bytes.\n");
-		
-// 		System.out.println("The Variable f is Float data type has the value of " + f + "\nSize of float: " + Float.BYTES + " bytes.");
-// 		System.out.println("Size of float: " + Float.SIZE + " bytes.\n");
-		
-
-// 		//Finding data type range minimum and maximum value
-// 		System.out.println("Minimum value of int range: " + Integer.MIN_VALUE + ".");
-// 		System.out.println("Maximum value for double range: " + Double.MAX_VALUE + ".");
-		
-		
-		
-		
-        
-//     }
-// }
 /**********************************************************************
   
   Program Title : Demonstrating Identifier Rules in Java
@@ -71,35 +12,51 @@
  
 // Intentionally incorrect program to demonstrate invalid Java identifiers.
 
-class SwitchStatus{
+// class switch{
+// 	String SwitchStatus = "On";
+	
+// }
+class SwitchClass{
 	String SwitchStatus = "On";
 	
 }
 
 public class IdentifierRules {
 	
-    public static void onestart() { 
+//     public static void 1start() { 
+// 		System.out.println("Static Method Output");
+// 	}
+	public static void onestart() { 
 		System.out.println("Static Method Output");
 	}
 
     public static void main(String[] args) {
 
+        // int 2ndPlace = 2;
         int secondPlace = 2;
 
+        // int first name = 10;
         int first_name = 10;
 
-        int price = 99;
+//         int price# = 99;
+// 		int user-name = 25;
+		int price = 99;
 		int user_name = 25;
 
+// 		int class = 1;
+// 		int double = 3;
 		int ClassVal = 1;
 		int DoubleVal = 3;
 
+        // int Score = 10;
+        // System.out.println(score);
         int Score = 10;
         System.out.println(Score);
 		
 		System.out.println("This program is executed after all errors are rectified.");
 
 
+        // 1start();
         onestart();
     }
 
